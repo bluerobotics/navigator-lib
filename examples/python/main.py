d4a4d6c84e9fab63f7e56aec8d3833b3de06f747
Raul Victor Trombin <raulvtrombin@gmail.com>
2023-05-10 14:20:02 -0300

    examples: python: Add example

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,6 +1,44 @@
+#!/usr/bin/env python
+
+import os
 import navigator
 
-tire = navigator.create_random_tire()
-print(f'Tire pressure: {tire.pressure}')
-print(f'Tire material: {tire.material}')
-print(f'Tire size: {tire.size.width}w, {tire.size.height}h')
+
+def navigator_check():
+    print(f"Fucntions available: {navigator.__all__}")
+
+    if os.environ.get("CI") == "true":
+        print("Running in CI")
+        print("Not possible to test navigator sensors yet.")
+        return
+
+    print("Initializing navigator module.")
+    navigator.init()
+
+    print("Setting leds on!")
+    navigator.set_led_on()
+
+    print(f"Temperature: {navigator.read_temp()}")
+
+    print(f"Pressure: {navigator.read_pressure()}")
+
+    Data = navigator.read_adc()
+    print(
+        f"Data ADC Channels: 1 = {Data.channel[0]}, 2 = {Data.channel[1]}, 3 = {Data.channel[2]}, 4 = {Data.channel[3]}"
+    )
+
+    Data = navigator.read_mag()
+    print(f"Magnetic field: X = {Data.x}, Y = {Data.y}, Z = {Data.z}")
+
+    Data = navigator.read_accel()
+    print(f"Acceleration: X = {Data.x}, Y = {Data.y}, Z = {Data.z}")
+
+    Data = navigator.read_gyro()
+    print(f"Gyroscope: X = {Data.x}, Y = {Data.y}, Z = {Data.z}")
+
+    print("Setting leds off!")
+    navigator.set_led_off()
+
+
+if __name__ == "__main__":
+    navigator_check()
