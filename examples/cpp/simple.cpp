45b8618ea5cdd6211ffd4b80f24e1e5a4b6e1a0a
Patrick José Pereira <patrickelectric@gmail.com>
2023-08-07 10:58:41 -0300

    examples: cpp: Fix type of print arguments
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/cpp/simple.cpp
+++ examples/cpp/simple.cpp
@@ -33,10 +33,10 @@ int main() {
   printf("Pressure: %f\n", read_pressure());
 
   ADCData adc = read_adc_all();
-  printf("Reading ADC Channels: 1 = %i, 2 = %i, 3 = %i, 4 = %i\n",
+  printf("Reading ADC Channels: 1 = %f, 2 = %f, 3 = %f, 4 = %f\n",
          adc.channel[0], adc.channel[1], adc.channel[2], adc.channel[3]);
 
-  printf("Data ADC Channels: 1 = %i\n", read_adc(AdcChannel::Ch1));
+  printf("Data ADC Channels: 1 = %f\n", read_adc(AdcChannel::Ch1));
 
   AxisData mag = read_mag();
   printf("Magnetic field: X = %f, Y = %f, Z = %f\n", mag.x, mag.y, mag.z);
